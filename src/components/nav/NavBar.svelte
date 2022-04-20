fc8c8860d2983aca8dfb7c37464a4718ecf2e9bb
Callum Harrod <callumharrod1994@hotmail.co.uk>
2022-04-20 12:54:01 +0100

    Added a couple new bits and some styling updates

--- src/components/nav/NavBar.svelte
+++ src/components/nav/NavBar.svelte
@@ -1,6 +1,17 @@
+<script>
+	import NavItem from './NavItem.svelte';
+	import ConnectWallet from '../ConnectWallet.svelte';
+	import Burger from '../icons/Burger.svelte';
+	import Logo from '../icons/Logo.svelte';
+	import Discord from '../icons/Discord.svelte';
+	import Twitter from '../icons/Twitter.svelte';
+	import Telegram from '../icons/Telegram.svelte';
+	export let sidebar = false;
+</script>
+
 <div class="absolute w-full">
-  <div
-      class="
+	<div
+		class="
         w-full
         max-w-screen-xl
         mx-auto
@@ -12,50 +23,35 @@
         
         {$$restProps.class}
       "
-    >
+	>
+		<!-- desktop nav-->
+		<div class="max-w-screen-2xl w-full mx-auto hidden md:flex">
+			<div class="mx-2 self-center py-auto mr-12">
+				<Logo />
+			</div>
+			<div class="ml-4 self-center flex">
+				<NavItem to="/" class="self-center">Home</NavItem>
+				<NavItem to="/biomes" class="self-center">Biomes</NavItem>
+				<NavItem to="/mint" class="self-center">Mint</NavItem>
+				<NavItem to="/marketplace" class="self-center">Marketplace</NavItem>
+			</div>
+			<div class="flex flex-row space-x-4 py-auto ml-auto items-center mr-10">
+				<Discord />
+				<Twitter />
+				<Telegram />
+			</div>
+			<ConnectWallet />
+		</div>
 
-      <!-- desktop nav-->
-      <div class="max-w-screen-2xl w-full mx-auto hidden md:flex">
-          <div class="mx-2 self-center py-auto mr-12"  >
-            <Logo />
-          </div >
-          <div class="ml-4 self-center flex">
-            <NavItem to="/" class="self-center"> Home </NavItem>
-            <NavItem to="/biomes" class="self-center"> Biomes </NavItem>
-            <NavItem to="/mint" class="self-center"> Mint </NavItem>
-            <NavItem to="/marketplace" class="self-center"> Marketplace </NavItem>
-          </div>
-          <div class="flex flex-row space-x-4 py-auto ml-auto items-center mr-10">
-            <Discord />
-            <Twitter />
-            <Telegram />
-          </div>
-          <ConnectWallet />
-      </div>
+		<!-- end desktop nav-->
 
-      <!-- end desktop nav-->
-
-
-      <div class="container mx-auto flex md:hidden">
-        <div class="mr-2 self-center py-auto"  >
-          <Logo />
-        </div >
-        <div class="md:hidden flex ml-auto p-4">
-            <Burger bind:open={sidebar} class="text-white inline-flex"/>
-        </div>
-      </div>
-  </div>
+		<div class="container mx-auto flex md:hidden">
+			<div class="mr-2 self-center py-auto">
+				<Logo />
+			</div>
+			<div class="md:hidden flex ml-auto p-4">
+				<Burger bind:open={sidebar} class="text-white inline-flex" />
+			</div>
+		</div>
+	</div>
 </div>
-
-
-
-<script>
-import NavItem from "./NavItem.svelte";
-import ConnectWallet from "../ConnectWallet.svelte";
-import Burger from "../icons/Burger.svelte";
-import Logo from "../icons/Logo.svelte";
-import Discord from "../icons/Discord.svelte";
-import Twitter from "../icons/Twitter.svelte";
-import Telegram from "../icons/Telegram.svelte";
-export let sidebar = false
-</script>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,97 +3,129 @@
 </script>
 
 <script lang="ts">
-	import MintInfo from "../components/MintInfo.svelte";
-	import RecentlyMinted from "../components/RecentlyMinted.svelte";
+	import MintInfo from '../components/MintInfo.svelte';
+	import RecentlyMinted from '../components/RecentlyMinted.svelte';
 
-	let pineForest = "src/assets/backgrounds/Pine forest.png"
-	let rocket = "src/assets/foregrounds/Cloud world.png"
-	let shroomland = "src/assets/foregrounds/Shroomland.png"
-	let spikyRidge = "src/assets/backgrounds/Spiky ridge.png"
+	// Backgrounds
+	let pineForest = 'src/assets/backgrounds/Pine forest.png';
+	let rocket = 'src/assets/foregrounds/Cloud world.png';
+	let shroomland = 'src/assets/foregrounds/Shroomland.png';
+	let spikyRidge = 'src/assets/backgrounds/Spiky ridge.png';
+	let floatingIsland = 'src/assets/compositions/1.png';
+
+	// Ducks
+	let duck1 = 'src/assets/ducks/1.png';
+	let duck2 = 'src/assets/foregrounds/2.png';
 </script>
 
 <svelte:head>
 	<title>Home</title>
 </svelte:head>
 
-<section class="flex flex-col justify-center items-center flex-1" style="background: linear-gradient(180deg, #48597E 76.3%, #3477B7 100%) ">
-	<div class="max-w-screen-xl flex mx-auto justify-center items-center h-72 flex-col text-white my-16">
-		<h1 class="w-full font-black text-7xl mt-40">
-			Welcome to Bizarre Biomes.
-		</h1>
-	
-		<h2 class="mt-5 text-xl">
-			An NFT project for duck holders, and newcomers alike.
-		</h2>
+<section
+	class="flex flex-col justify-center items-center flex-1"
+	style="background: linear-gradient(180deg, #48597E 76.3%, #3477B7 100%) "
+>
+	<div class="max-w-screen-xl flex mx-auto justify-center items-center flex-col text-white my-16">
+		<h1 class="w-full font-black text-7xl mt-[230px]">Welcome to Bizarre Biomes.</h1>
+
+		<h2 class="mt-5 text-xl">An NFT project for duck holders, and newcomers alike.</h2>
 		<MintInfo class="mt-5" />
 	</div>
 </section>
 
-<img src="{pineForest}" alt="Pine Forest" />
+<img src={pineForest} alt="Pine Forest" />
 
 <section style="background: #50cf7d" class="py-28">
-	<div class="max-w-screen-xl mx-auto grid grid-cols-2">
+	<div class="max-w-screen-xl mx-auto grid grid-cols-2 items-center">
 		<div>
 			<RecentlyMinted class="border-4 border-gray-700 rounded" />
 		</div>
-		<div class="p-28">
-			<h2 class="text-4xl mb-5 font-semibold text-white">From the skyline to the pits of Mordor.</h2>
-			<p class="font-normal text-base text-white">Welcome to the newest project from the team behind Ducks and Zilkroad. A collection of 2000 different biomes.</p>
+		<div class="px-28">
+			<h2 class="text-4xl mb-5 font-semibold text-white">
+				From the skyline to the pits of Mordor.
+			</h2>
+			<p class="font-normal text-base text-white">
+				Welcome to the newest project from the team behind Ducks and Zilkroad. A collection of 2000
+				different biomes.
+			</p>
 			<div class="flex flex-row my-5 gap-x-5">
 				<button class="btn btn-primary">Mint a new biome</button>
 				<button class="btn btn-secondary">View all biomes</button>
 			</div>
-		</div>	
+		</div>
 	</div>
 </section>
 
-<img src="{rocket}" alt="rocket"  style="background: #50cf7d;" />
+<img src={rocket} alt="rocket" style="background: #50cf7d;" />
 
-<div class="h-16" style="background: linear-gradient(180deg, #E6F1F2 0%, #CCEEF7 100%);"></div>
+<div class="h-16" style="background: linear-gradient(180deg, #E6F1F2 0%, #CCEEF7 100%);" />
 
-<img src="{shroomland}" alt="shroomland"  style="background: #CCEEF7;"/>
+<img src={shroomland} alt="shroomland" style="background: #CCEEF7;" />
 
 <section style="background: #A1A1E4;">
 	<div class="max-w-screen-xl mx-auto py-28">
 		<div class="max-w-screen-xl flex mx-auto justify-center items-center flex-col text-white ">
-			<h3 class="font-semibold text-4xl ">
+			<h1 class="font-semibold text-4xl max-w-[496px] text-center">
 				A title on the latest NFTs minted
-			</h3>
-		
-			<h2 class="mt-5 text-base">
-				Welcome to the newest project from the team behind Ducks and Zilkroad. A collection of 2000 different worlds from the multiverse.
-			</h2>
-	
+			</h1>
+			<p class="mt-5 text-base max-w-[600px] text-center">
+				Welcome to the newest project from the team behind Ducks and Zilkroad. A collection of 2000
+				different worlds from the multiverse.
+			</p>
 		</div>
 		<RecentlyMinted class="mt-20" />
 	</div>
 </section>
 
+<div class="h-16" style="background: linear-gradient(180deg, #A1A1E4 0%, #FCB698 100%);" />
 
-<div class="h-16" style="background: linear-gradient(180deg, #A1A1E4 0%, #FCB698 100%);"></div>
-
-<img src="{spikyRidge}" alt="spiky ridge"  style="background: #FCB698;"/>
-
+<img src={spikyRidge} alt="spiky ridge" style="background: #FCB698;" />
+floatingIsland
 <section style="background: #E4CF81;">
 	<div class="max-w-screen-xl mx-auto py-28 text-primary">
 		<div class="max-w-screen-xl flex mx-auto justify-center items-center flex-col  ">
-			<h3 class="font-semibold text-4xl ">
-			Frequently asked questions
-			</h3>
-		
-			<h2 class="mt-5 text-base">
-				Welcome to the newest project from the team behind Ducks and Zilkroad. A collection of 2000 different worlds from the multiverse.
-			</h2>
+			<h1 class="font-semibold text-4xl max-w-[496px] text-center">Frequently asked questions</h1>
+			<p class="mt-5 text-base max-w-[600px] text-center">
+				Welcome to the newest project from the team behind Ducks and Zilkroad. A collection of 2000
+				different worlds from the multiverse.
+			</p>
 		</div>
 		<div class="grid grid-cols-2 gap-6 mt-20">
 			{#each Array(7) as i}
-			<div>
-				<h4 class="font-semibold text-2xl">FAQ title.</h4>
-				<p class="mt-5 ">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vulputate risus non diam sit quis quisque. Purus commodo aliquet justo, ipsum eu tortor quis vitae.</p>
-			</div>
+				<div>
+					<h2 class="font-semibold text-2xl">FAQ title.</h2>
+					<p class="mt-5 ">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vulputate risus non diam sit
+						quis quisque. Purus commodo aliquet justo, ipsum eu tortor quis vitae.
+					</p>
+				</div>
 			{/each}
 		</div>
 	</div>
 </section>
 
-<div class="h-28" style="background: linear-gradient(180deg, #E4CF81 0%, #FDC3CB 100%);"></div>
+<div class="h-28" style="background: linear-gradient(180deg, #E4CF81 0%, #FDC3CB 100%);" />
+<img src={floatingIsland} alt="Floating island" style="background: #9DD380;" />
+<section style="background: #9DD380;">
+	<div class="max-w-screen-xl mx-auto py-28 text-primary">
+		<div class="max-w-screen-xl flex mx-auto justify-center items-center flex-col  ">
+			<h1 class="font-semibold text-4xl text-white max-w-[496px] text-center">
+				A creation from the team behind Ducks.
+			</h1>
+			<p class="mt-5 text-base max-w-[600px] text-center text-white">
+				For a great project, you need a great team. Luckily, we have that with these fine people
+				shown below.
+			</p>
+		</div>
+		<div class="grid grid-cols-4 gap-6 mt-20">
+			{#each Array(8) as i}
+				<div class="bg-[#90C772] flex flex-col items-center justify-center p-5">
+					<img src={duck1} alt="Floating island" class="max-w-[100%]" />
+					<h3 class="text-white text-xl mb-[10px] font-semibold">Nines</h3>
+					<p class="text-white">Developer</p>
+				</div>
+			{/each}
+		</div>
+	</div>
+</section>
