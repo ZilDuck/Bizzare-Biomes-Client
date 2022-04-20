4dca008ca11eee4a9cce96993e59fd990c1ca1ee
neeko <59419317+GregzzGamz@users.noreply.github.com>
2022-04-20 18:07:26 +0100

    Progress bar component

---
+++ src/components/ProgressBar.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    export let current:number 
+    export let total:number
+</script>
+
+<div class="{$$restProps.class}">
+    <div class="overflow-hidden h-2 text-xs flex rounded bg-gray-200">
+        <div style="width:{(current/total) * 100}%" class="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-gray-400"></div>
+    </div>
+</div>
