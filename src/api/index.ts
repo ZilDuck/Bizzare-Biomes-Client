47179723caa87b935fee4a106224a71951cd0fe2
prodpeak <dtudor01@gmail.com>
2022-06-16 13:52:22 +0100

    Update biomes

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,7 +3,7 @@ const baseURL = process.env.BIOMES_API ?? import.meta.env.VITE_BIOMES_API
 type endPoints = 'biomes-list'|'biomes'|string
 
 const get = async (point: endPoints) => {
-  const res = await fetch(`http://staging-api.bizarrebiomes.com/${point}`);
+  const res = await fetch(`${baseURL}/${point}`);
   return res.json()
 } 
 
