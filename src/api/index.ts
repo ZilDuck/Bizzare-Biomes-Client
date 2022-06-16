019121ed1c55be557cb188c9a75cf1807156e4a3
prodpeak <dtudor01@gmail.com>
2022-06-16 12:34:11 +0100

    Fix env

--- src/api/index.ts
+++ src/api/index.ts
@@ -5,9 +5,11 @@ type endPoints = 'biomes-list'|'biomes'|string
 const get = async (point: endPoints) => {
   console.log(import.meta.env.VITE_BIOMES_API)
   console.log(process.env.BIOMES_API);
-  console.log(baseURL);
-  const res = fetch(`${baseURL}/${point}`)
-  return (await res).json()
+  console.log(`${baseURL}/${point}`);
+  const resp = await fetch(`${baseURL}/${point}`);
+  const body = await resp.text();
+  console.log(body)
+  return JSON.parse(body)
 } 
 
 const API ={
