dca55e414424702d1bf81c44a476eee1e71096ee
prodpeak <dtudor01@gmail.com>
2022-06-16 09:27:49 +0100

    Fix env

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,14 +1,8 @@
-import { variables } from '../variables.js'
-
-export const { biomesApi } = variables
-
-const baseURL:string = biomesApi
+const baseURL:string = import.meta.env.VITE_BIOMES_API
 
 type endPoints = 'biomes-list'|'biomes'|string
 
 const get = async (point: endPoints) => {
-  console.log(`${baseURL}/${point}`)
-
   const res = fetch(`${baseURL}/${point}`)
   return (await res).json()
 } 
--- src/variables.ts
+++
@@ -1,3 +0,0 @@
-export const variables = {
-    biomesApi: import.meta.env.VITE_BIOMES_API ?? import.meta.env.BIOMES_API,
-}
