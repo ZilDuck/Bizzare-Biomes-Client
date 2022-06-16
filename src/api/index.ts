4914255ac45bef81b8dd458f856b16a46f99e41a
prodpeak <dtudor01@gmail.com>
2022-06-16 12:20:39 +0100

    Fix env

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,10 +1,11 @@
-const baseURL:string = import.meta.env.VITE_BIOMES_API ?? process.env.BIOMES_API
+const baseURL:string = process.env.BIOMES_API ?? import.meta.env.VITE_BIOMES_API
 
 type endPoints = 'biomes-list'|'biomes'|string
 
 const get = async (point: endPoints) => {
   console.log(import.meta.env.VITE_BIOMES_API)
   console.log(process.env.BIOMES_API);
+  console.log(baseURL);
   const res = fetch(`${baseURL}/${point}`)
   return (await res).json()
 } 
