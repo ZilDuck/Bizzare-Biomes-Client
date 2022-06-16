10ddd38819759bbcf64698db85fa47f23a08b2e9
prodpeak <dtudor01@gmail.com>
2022-06-16 13:20:54 +0100

    Update biomes

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,9 +3,6 @@ const baseURL:string = process.env.BIOMES_API ?? import.meta.env.VITE_BIOMES_API
 type endPoints = 'biomes-list'|'biomes'|string
 
 const get = async (point: endPoints) => {
-  console.log(import.meta.env.VITE_BIOMES_API)
-  console.log(process.env.BIOMES_API);
-  console.log(`${baseURL}/${point}`);
   const res = await fetch(`${baseURL}/${point}`);
   return res.json()
 } 
