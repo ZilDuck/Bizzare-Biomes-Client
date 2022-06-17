88249f128b610116338c25efc70bcc0805354bc3
prodpeak <dtudor01@gmail.com>
2022-06-17 21:10:36 +0100

    Hard code staging api

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,14 +1,8 @@
-const baseURL = process.env.BIOMES_API ?? import.meta.env.VITE_BIOMES_API
-const processApi = process.env.BIOMES_API
-const envApi = import.meta.env.BIOMES_API
-const envViteApi = import.meta.env.VITE_BIOMES_API
+const baseURL = 'http://staging-api.bizarrebiomes.com'
 
 type endPoints = 'biomes-list'|'biomes'|string
 
 const get = async (point: endPoints) => {
-  console.log(processApi)
-  console.log(envApi)
-  console.log(envViteApi)
   const res = await fetch(`${baseURL}/${point}`);
   return res.json()
 } 
